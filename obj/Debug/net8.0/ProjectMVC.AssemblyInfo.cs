adfe59cbda18e4cedb7f8b8939af3a0a948591ee
yamakazeAnkk <tuongankk2003@gmail.com>
2025-05-21 21:20:53 +0700

    add ed

--- obj/Debug/net8.0/ProjectMVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98740ade797280ce766c71cebdaa28d9a5e73104")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
